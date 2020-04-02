1dc5238fd2e98af96c68c6ea45dcf2f489f8775e
lorenzo <thelorenzo533@gmail.com>
2020-04-02 21:50:17 +0200

    Add vue component

---
+++ resources/components/comptes.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h1>Bonjour</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "comptes"
+    }
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,15 @@
 require('./bootstrap');
+
+
+window.Vue = require('vue');
+
+//Vue.component('example-component', require('./components/ExempleComponent.vue').default);
+//Exemple d'importation de composants. Uniquement requis pour les composants globaux.
+
+Vue.component('comptes', require('../components/comptes.vue').default);
+
+const app = new Vue({
+    el: '#app'
+});
+
+console.log(app);
